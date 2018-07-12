eadc17f082f3463a3183536e39d3dec075b87944
Jing Cao <jingcao.yale@gmail.com>
2018-07-12 16:12:04 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,4 +8,8 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?
+  if () 
+    return true
+  else 
+    return false
 end
