7c9c1f230e95294d35a292e3d51eecc99197aea3
Jing Cao <jingcao.yale@gmail.com>
2018-07-12 16:32:18 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if position_taken?(board,index) == false && 0 <= index <= 8
+  if position_taken?(board,index) == false && index <= 8 && index >= 0
     return true
   end
 end
